a703b042e54d53609379b26888e89bfcc34218a5
thecatisbest <102031694+thecatisbest@users.noreply.github.com>
2022-06-27 17:57:57 +0800

    Add Permission

--- src/main/java/me/FKey/Command/Commands.java
+++ src/main/java/me/FKey/Command/Commands.java
@@ -5,7 +5,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 
 
@@ -15,35 +14,32 @@ public class Commands implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
         if (label.equalsIgnoreCase("FKey")) {
-            if (!(sender instanceof Player)) {
-                //console
-                sender.sendMessage("The console cannot run this command");
-                return true;
-            }
-            Player player = (Player) sender;
-            if (args.length == 0) {
-                // /F-Key
-                player.sendMessage(ChatColor.RED + "Use '/FKey help' to view all commands");
-                return true;
-            }
-            if (args.length >= 1) {
-                if (args[0].equalsIgnoreCase("reload")) {
-                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&aStarting reload... &e(If not working download Plugman plugin)"));
-                    player.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "plugman reload F-Key");
-                    return true;
+
+                if (sender.hasPermission("fkey.Commands.FKey")) {
+                    if (args.length == 0) {
+                        // /F-Key
+                        sender.sendMessage(ChatColor.RED + "Use '/FKey help' to view all commands");
+                        return true;
+                    }
+                    if (args.length == 1) {
+                        if (args[0].equalsIgnoreCase("reload")) {
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&aStarting reload... &e(If not working download Plugman plugin)"));
+                            sender.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "plugman reload F-Key");
+                            return true;
+                        }
+                        if (args[0].equalsIgnoreCase("help")) {
+
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&b[F-Key] commands"));
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&aThank you for using my plugin! : )"));
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&e/FKey reload"));
+
+                        }
+                    }
+
+                } else {
+                    sender.sendMessage(ChatColor.RED + "You do not have permission!");
                 }
-                if (args[0].equalsIgnoreCase("help")) {
-
-                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&b[F-Key] commands"));
-                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&aThank you for using my plugin! (●'◡'●)"));
-                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&e/FKey reload"));
-
-                    return true;
-                }
-            }
-
         }
-        return false;
+        return true;
     }
 }
-
